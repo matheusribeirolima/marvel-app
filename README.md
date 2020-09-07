89ad65fe74a3a312d04bbc60c7fdfca826e6dd2a
matheusribeirolima <matheus_ribeirolima@hotmail.com>
2020-09-06 23:46:08 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+<p align="center">
+    <img alt="Marvel App Logo" src="/images/marvel.svg" width=250px/>
+</p>
+
+# Marvel App
+
+![Project Checks](https://github.com/matheusribeirolima/marvel-app/workflows/.github/workflows/android-master.yml/badge.svg) ![License](https://img.shields.io/github/license/cortinico/kotlin-android-template.svg)
+
+A simple Android project that lists and details Marvel characters.
+
+## How to use 👣
+
+Just add the `key.properties` file to the root of the project with the `API_KEY` attribute filled ith your **Marvel API** public key.
+
+## Features 🎨
+
+- Paginated listing of the characters' name and image.
+- Display of the description and most expensive HQ of the character that the user selects.
+
+## Tools 🔨
+
+This project was built in Kotlin and designed according to the MVVM and Clean architectures using:
+
+- **ViewModel:** The [ViewModel](https://developer.android.com/topic/libraries/architecture/viewmodel) class is designed to store and manage UI-related data in a lifecycle conscious way.
+- **LiveData:** [LiveData](https://developer.android.com/topic/libraries/architecture/livedata) is an observable data holder class. Unlike a regular observable, LiveData is lifecycle-aware, meaning it respects the lifecycle of other app components, such as activities, fragments, or services.
+- **Coroutines:** [Coroutines](https://kotlinlang.org/docs/reference/coroutines-overview.html) not only open the doors to asynchronous programming, but also provide a wealth of other possibilities such as concurrency, actors, etc.
+- **Dagger Hilt:** [Hilt](https://developer.android.com/training/dependency-injection/hilt-android) is a dependency injection library for Android that reduces the boilerplate of doing manual dependency injection in your project.
+- **Material Components:** [Material Components](https://material.io/components) are interactive building blocks for creating a user interface.
+- **Navigation:** [Android Jetpack's Navigation](https://developer.android.com/guide/navigation) component helps you implement navigation, from simple button clicks to more complex patterns, such as app bars and the navigation drawer.
+- **Paging:** The [Paging Library](https://developer.android.com/topic/libraries/architecture/paging) helps you load and display small chunks of data at a time.
+- **Retrofit:** A [type-safe HTTP client](https://square.github.io/retrofit/) for Android and the JVM
+- **Coil:** [Image loading](https://coil-kt.github.io/coil/) for Android backed by Kotlin Coroutines.
+- **Mockk:** [Mocking library](https://mockk.io) for Kotlin
+
+## Static Analysis 🔍
+
+This project uses [**detekt**](https://github.com/detekt/detekt) to analyze the source code, with the configuration that is stored in the [detekt.yml](config/detekt/detekt.yml) file.
+
+## CI ⚙️
+
+This project is using [**GitHub Actions**](https://github.com/matheusribeirolima/marvel-app/actions) as CI.
+
+Currently, the following workflow is available:
+- [Project Checks](.github/workflows/android-master.yml) - Perform static code validations, run tests and assemble the project.
+
+## Marvel Api 📦
+
+[![Open API](􏰓􏰃􏰃􏰔􏰂􏰑􏰕􏰕􏰆􏰁􏰖􏰁􏰗􏰋􏰔􏰁􏰇􏰘􏰌􏰅􏰇􏰖􏰁􏰗􏰘􏰙􏰋􏰌􏰕􏰓􏰃􏰃􏰔􏰂􏰑􏰕􏰕􏰆􏰁􏰖􏰁􏰗􏰋􏰔􏰁􏰇􏰘􏰌􏰅􏰇􏰖􏰁􏰗􏰘􏰙􏰋􏰌􏰕􏰓􏰃􏰃􏰔􏰂􏰑􏰕􏰕􏰆􏰁􏰖􏰁􏰗􏰋􏰔􏰁􏰇􏰘􏰌􏰅􏰇􏰖􏰁􏰗􏰘􏰙􏰋􏰌􏰕􏰓􏰃􏰃􏰔􏰂􏰑􏰕􏰕􏰆􏰁􏰖􏰁􏰗􏰋􏰔􏰁􏰇􏰘􏰌􏰅􏰇􏰖􏰁􏰗􏰘􏰙􏰋􏰌􏰕􏰓􏰃􏰃􏰔􏰂􏰑􏰕􏰕􏰆􏰁􏰖􏰁􏰗􏰋􏰔􏰁􏰇􏰘􏰌􏰅􏰇􏰖􏰁􏰗􏰘􏰙􏰋􏰌􏰕https://developer.marvel.com)] that provides information about Marvel Comics heroes.
+
+## Contributing 🤝
+
+Feel free to open a issue or submit a pull request for any bugs/improvements.
---
+++ buildSrc/src/main/kotlin/com/marvel/app/Dependencies.kt
@@ -0,0 +1,96 @@
+package com.marvel.app
+
+object Dependencies {
+    object Application {
+        const val id = "com.marvel.app"
+    }
+
+    object Modules {
+        const val dependencies = ":dependencies"
+    }
+
+    object Releases {
+        const val versionCode = 1
+        const val versionName = "1.0"
+    }
+
+    object Versions {
+        const val compileSdk = 29
+        const val buildTools = "30.0.2"
+        const val minSdk = 21
+        const val targetSdk = 29
+        const val gradle = "4.0.1"
+        const val kotlin = "1.4.0"
+        const val hilt = "2.28.3-alpha"
+        const val core = "1.3.1"
+        const val appCompat = "1.2.0"
+        const val constraintLayout = "2.0.1"
+        const val recyclerView = "1.1.0"
+        const val lifecycle = "2.2.0"
+        const val arch = "2.1.0"
+        const val hiltX = "1.0.0-alpha02"
+        const val coroutines = "1.3.9"
+        const val navigation = "2.3.0"
+        const val material = "1.2.1"
+        const val paging = "3.0.0-alpha06"
+        const val coil = "0.13.0"
+        const val retrofit = "2.9.0"
+        const val logging = "4.8.1"
+        const val junit = "4.13"
+        const val mockk = "1.10.0"
+        const val testRules = "1.3.0"
+        const val testExt = "1.1.2"
+        const val espresso = "3.3.0"
+    }
+
+    object Plugins {
+        const val gradle = "com.android.tools.build:gradle:${Versions.gradle}"
+        const val kotlin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+        const val hilt = "com.google.dagger:hilt-android-gradle-plugin:${Versions.hilt}"
+        const val navigation = "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}"
+    }
+
+    object GeneralLibraries {
+        const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+        const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.coroutines}"
+        const val coroutinesAndroid = "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutines}"
+        const val coil = "io.coil-kt:coil:${Versions.coil}"
+        const val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+        const val retrofitGson = "com.squareup.retrofit2:converter-gson:${Versions.retrofit}"
+        const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.logging}"
+    }
+
+    object GoogleLibraries {
+        const val hilt = "com.google.dagger:hilt-android:${Versions.hilt}"
+        const val hiltCompiler = "com.google.dagger:hilt-android-compiler:${Versions.hilt}"
+        const val materialDesign = "com.google.android.material:material:${Versions.material}"
+    }
+
+    object AndroidxLibraries {
+        const val core = "androidx.core:core-ktx:${Versions.core}"
+        const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+        const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+        const val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerView}"
+        const val viewModel = "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.lifecycle}"
+        const val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle}"
+        const val hiltX = "androidx.hilt:hilt-lifecycle-viewmodel:${Versions.hiltX}"
+        const val hiltXCompiler = "androidx.hilt:hilt-compiler:${Versions.hiltX}"
+        const val navigationFragment = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+        const val navigationUi = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+        const val paging = "androidx.paging:paging-runtime:${Versions.paging}"
+    }
+
+    object TestLibraries {
+        const val junit = "junit:junit:${Versions.junit}"
+        const val mockk = "io.mockk:mockk:${Versions.mockk}"
+        const val archCoreTesting = "androidx.arch.core:core-testing:${Versions.arch}"
+        const val paging = "androidx.paging:paging-common:${Versions.paging}"
+        const val hilt = "com.google.dagger:hilt-android-testing:${Versions.hilt}"
+        const val hiltCompiler = "com.google.dagger:hilt-android-compiler:${Versions.hilt}"
+        const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutines}"
+        const val testRules = "androidx.test:rules:${Versions.testRules}"
+        const val testExt = "androidx.test.ext:junit:${Versions.testExt}"
+        const val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+        const val navigation = "androidx.navigation:navigation-testing:${Versions.navigation}"
+    }
+}
