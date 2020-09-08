edca53bb386254c2b0fd36ea5f5f2bb2a50bdad9
matheusribeirolima <matheus_ribeirolima@hotmail.com>
2020-09-08 00:44:53 -0300

    Added constructor calls

--- features/detail/src/main/java/com/marvel/detail/DetailActivity.kt
+++ features/detail/src/main/java/com/marvel/detail/DetailActivity.kt
@@ -3,7 +3,7 @@ package com.marvel.detail
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class DetailActivity : AppCompatActivity {
+class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- features/home/src/main/java/com/marvel/home/HomeActivity.kt
+++ features/home/src/main/java/com/marvel/home/HomeActivity.kt
@@ -3,7 +3,7 @@ package com.marvel.home
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class HomeActivity : AppCompatActivity {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
